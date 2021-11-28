22afe1ba68865f7cf31c362c2425553b1c6dfcc2
Arthur Silva <arthur1silva1r@gmail.com>
2021-11-28 11:25:38 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -12,19 +12,50 @@ class App extends React.Component {
       cardAttr2: 0,
       cardAttr3: 0,
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: false,
+      isSaveButtonDisabled: true,
     };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.disableSaveButton = this.disableSaveButton.bind(this);
+  }
+
+  // eslint-disable-next-line react/sort-comp
+  verifyAttrs = () => {
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const arrayAttrs = [cardAttr1, cardAttr2, cardAttr3];
+    const limitUnit = 90;
+    const limitSum = 210;
+    let control = false;
+    const isValidAttr = arrayAttrs.every((attr) => Number(attr) <= limitUnit);
+    if (Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= limitSum) {
+      control = true;
+    }
+    const positiveNumbers = arrayAttrs.every((attr) => Number(attr) >= 0);
+    return positiveNumbers && isValidAttr && control;
+  }
+
+  verifyInputs = () => {
+    const { cardName, cardImage, cardDescription, cardRare } = this.state;
+    const arrayInputs = [cardName, cardImage, cardDescription, cardRare];
+    const notEmpty = arrayInputs.every((input) => input.length > 0);
+    return notEmpty;
   }
 
   onInputChange({ target: { value, name, type, checked } }) {
-    console.log('teste');
     const result = type === 'checkbox' ? checked : value;
     this.setState({
       [name]: result,
-    });
+    }, this.disableSaveButton);
+  }
+
+  disableSaveButton() {
+    if (this.verifyAttrs() && this.verifyInputs()) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
   }
 
   render() {
